f4116b86f31c0caaa117d964ec21fafb2d3f99cd
leejaeyoung <kltop1210@gmail.com>
2025-08-29 21:21:33 +0900

    패키지 추가 및 Ray검사 추가

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,12 +5,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private Animator animator;
     private PlayerInput input;
-    private Camera maincamera;
-    private LayerMask groundMask;
-
-    public Collider Basecollider;
+    
+    private LayerMask groundMask;    
    
-
     public float moveSpeed = 8f;    
     private bool isGround = false;
     private Vector3 pos;
@@ -20,31 +17,25 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         input = GetComponent<PlayerInput>();
-        animator = GetComponent<Animator>();        
-        maincamera = Camera.main;
+        animator = GetComponent<Animator>();                
         groundMask = LayerMask.GetMask("Base");
 
     }
     
     private void FixedUpdate()
     {
-
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);        
         //RaycastHit hit;
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            Vector3 mouseDir = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            
+        if (Physics.Raycast(ray, out RaycastHit hit , float.MaxValue , groundMask))
+        {            
+            Vector3 mouseDir = new Vector3(hit.point.x, transform.position.y, hit.point.z);            
             transform.LookAt(mouseDir);
-
         }
 
         var distanceX = input.MoveV * moveSpeed * Time.deltaTime;
         var distanceZ = input.MoveH * moveSpeed * Time.deltaTime;
 
         rb.MovePosition(transform.position + distanceX * transform.forward + distanceZ * transform.right);
-
 
         // ( "피라미터 이름" /  값)
         animator.SetFloat("moveX", input.MoveV);
